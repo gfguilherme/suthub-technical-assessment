f5db6bbdcb609327b362910d21474387e4bb76b2
Guilherme Ferreira <guilherme.ferreira@4linux.com.br>
2024-05-30 10:57:07 -0300

    feat: add endpoint to delete an age group
    - Bump version from 0.2.0 to 0.3.0

--- age-groups-api/src/api/routes/age_groups.py
+++ age-groups-api/src/api/routes/age_groups.py
@@ -1,5 +1,6 @@
 import logging
 
+from botocore.exceptions import ClientError
 from fastapi import APIRouter, HTTPException, status
 
 from src.db import age_groups_table
@@ -23,4 +24,33 @@ def create_age_group(age_group: AgeGroup) -> AgeGroup:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="An unexpected error occurred while creating an age group. Please try again later.",
+        )
+
+
+@router.delete("/{name}", status_code=status.HTTP_200_OK)
+def delete_age_group(name: str) -> AgeGroup:
+    """Send a DELETE request to this resource to delete an age group."""
+
+    try:
+        age_group = age_groups_table.delete_item(
+            Key={"name": name},
+            ReturnValues="ALL_OLD",
+            ConditionExpression="attribute_exists(#n)",
+            ExpressionAttributeNames={"#n": "name"},
+        )
+        return age_group["Attributes"]
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "ConditionalCheckFailedException":
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"The age group with the name '{name}' was not found.",
+            )
+        else:
+            raise
+    except Exception as exc:
+        logger.error("Failed to delete the age group: %s", exc)
+
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An unexpected error occurred while deleting the age group. Please try again later.",
         )
