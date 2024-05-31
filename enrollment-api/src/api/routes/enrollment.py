75447a5d8c197b2970dc432024075741d349ac4d
Guilherme Ferreira <guilherme.ferreira@4linux.com.br>
2024-05-31 01:53:47 -0300

    feat: Add endpoint to read enrollment by CPF
    - Bump version from 0.2.0 to 0.3.0

--- enrollment-api/src/api/routes/enrollment.py
+++ enrollment-api/src/api/routes/enrollment.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 
 from src.api.deps import get_age_group, get_current_user
 from src.config import settings
-from src.db import sqs
+from src.db import enrollments_table, sqs
 from src.models import Enrollment
 
 logger = logging.getLogger(__name__)
@@ -30,4 +30,31 @@ def create_enrollment(enrollment: Enrollment = Depends(get_age_group)) -> Enroll
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="An unexpected error occurred while creating the enrolment. Please try again later.",
+        )
+
+
+@router.get(
+    "/{CPF}", status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user)]
+)
+def read_enrollment(CPF: str) -> Enrollment:
+    """Send a GET request to this resource to retrieve an enrollment by CPF."""
+
+    try:
+        response = enrollments_table.get_item(Key={"CPF": CPF})
+
+        if "Item" not in response:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Enrollment not found or not yet processed. Please try again later.",
+            )
+
+        return response["Item"]
+    except HTTPException as exc:
+        raise exc
+    except Exception as exc:
+        logger.error(f"Failed to retrieve enrollment from DynamoDB: {exc}")
+
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An unexpected error occurred while retrieving the enrolment. Please try again later.",
         )
